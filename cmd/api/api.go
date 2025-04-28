cd196fc0343b873330fb4a32055a74fd1ea09f3f
afirthes <afirthes@gmail.com>
2025-04-28 15:54:01 +0300

    Auto reloadint by air.

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -18,8 +18,15 @@ type application struct {
 
 func (app *application) mount() http.Handler {
 	r := chi.NewRouter()
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
+
+	// Timeout on request context (ctx), that will signal
+	// through ctx.Done() that the request has time out and further
+	// processing should be stopped.
+	r.Use(middleware.Timeout(60 * time.Second))
 
 	r.Route("/v1", func(r chi.Router) {
 		r.Get("/health", app.healthHandler)
